e1afec99df75e9804ac645e5159cdcc3b75ad149
RichardCheung1 <li.cheung@mail.mcgill.ca>
2016-08-24 19:14:46 -0400

    entered information

--- js/main.js
+++ js/main.js
@@ -1,37 +1,43 @@
 'use strict';
 
-function navColorChange() {
-  var navbar = $('nav');
-  var innerHeight = $('#overlayView').innerHeight();
-  var offset = $('#overlayView').offset().top;
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > innerHeight - offset) {
-      navbar.wrap('  <div class="navbar-fixed"> </div>');
-      navbar.addClass('green darken-1');
-    } else {
-      navbar.removeClass('green darken-1');
-    }
-  });
-}
+// function navColorChange() {
+//   var navbar = $('nav');
+//   var innerHeight = $('#overlayView').innerHeight();
+//   var offset = $('#overlayView').offset().top;
+//   $(window).scroll(function () {
+//     // if ($(window).scrollTop() > innerHeight - offset) {
+//     //   navbar.wrap('  <div class="navbar-fixed"> </div>');
+//     //   navbar.addClass('green darken-1');
+//     // } else {
+//     //   navbar.removeClass('green darken-1');
+//     // }
+//   });
+// }
+//
+// function navWrapChange() {
+//   var navbar = $('nav');
+//   $(window).scroll(function () {
+//
+//     //console.log(!navbar.parent().is(".overlay"));
+//     // if (navbar.parent().is("div")) {
+//     //   navbar.unwrap();
+//     // }
+//   });
+// }
+//
+// var options = [
+//   {
+//     selector: 'nav', offset: 200, callback: navWrapChange()
+//   },
+//   {
+//     selector: 'nav', offset: 200, callback: navColorChange()
+//   }
+// ];
+// Materialize.scrollFire(options);
 
-function navWrapChange() {
-  var navbar = $('nav');
-  $(window).scroll(function () {
 
-    //console.log(!navbar.parent().is(".overlay"));
-    // if (navbar.parent().is("div")) {
-    //   navbar.unwrap();
-    // }
-  });
-}
-
-var options = [
-  {
-    selector: 'nav', offset: 200, callback: navWrapChange()
-  },
-  {
-    selector: 'nav', offset: 200, callback: navColorChange()
+$('.button-collapse').sideNav({
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
   }
-];
-Materialize.scrollFire(options);
-
+);
