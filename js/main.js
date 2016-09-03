4468e40e11b0b42b0a954adb1ccf4b1d17c57e9e
RichardCheung1 <li.cheung@mail.mcgill.ca>
2016-09-03 19:11:20 -0400

    about me section

--- js/main.js
+++ js/main.js
@@ -1,40 +1,5 @@
 'use strict';
 
-// function navColorChange() {
-//   var navbar = $('nav');
-//   var innerHeight = $('#overlayView').innerHeight();
-//   var offset = $('#overlayView').offset().top;
-//   $(window).scroll(function () {
-//     // if ($(window).scrollTop() > innerHeight - offset) {
-//     //   navbar.wrap('  <div class="navbar-fixed"> </div>');
-//     //   navbar.addClass('green darken-1');
-//     // } else {
-//     //   navbar.removeClass('green darken-1');
-//     // }
-//   });
-// }
-//
-// function navWrapChange() {
-//   var navbar = $('nav');
-//   $(window).scroll(function () {
-//
-//     //console.log(!navbar.parent().is(".overlay"));
-//     // if (navbar.parent().is("div")) {
-//     //   navbar.unwrap();
-//     // }
-//   });
-// }
-//
-// var options = [
-//   {
-//     selector: 'nav', offset: 200, callback: navWrapChange()
-//   },
-//   {
-//     selector: 'nav', offset: 200, callback: navColorChange()
-//   }
-// ];
-// Materialize.scrollFire(options);
-
 
 $('.button-collapse').sideNav({
     edge: 'left', // Choose the horizontal origin
